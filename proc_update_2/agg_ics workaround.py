153b561d20a473dd29d9f55af3e032dc22c50d7e
NMojgani <mojganin@uci.edu>
2024-03-14 12:44:39 -0700

    Update agg_ics workaround.py

--- proc_update_2/agg_ics workaround.py
+++ proc_update_2/agg_ics workaround.py
@@ -1,4 +1,5 @@
 # only aggregating 2 things at a time
+import cerberus
 refs = [False for i in range(2)]
 prev_ics = "analysis/240221_nm/cerberus/agg/ad004_hTREM2KI-WT_M_8_months_HC_6_ic.tsv"
 cur_ics = "/share/crsp/lab/model-ad/share/freese/modelad_pipeline/proc_update_2/data/cerberus/ad004/hTREM2KI-WT_M_8_weeks_HC_ic.tsv"
